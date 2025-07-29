aa2b4e0653c52891ab840be7a3b4b9c812826bc7
gazooo0 <fukugazu@gmail.com>
2025-07-29 22:27:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ def match_umamusume(pedigree_dict):
         if key in normalized_umamusume:
             img_url = image_dict.get(name, "")
             if img_url:
-                matched.append(f"<img src='{img_url}' width='100' style='vertical-align:middle;margin-right:8px;'>【{pos}】{name}")
+                matched.append(f"<img src='{img_url}' width='70' style='vertical-align:middle;margin-right:8px;'>{pos}/{name}")
             else:
                 matched.append(f"【{pos}】{name}")
     return matched
